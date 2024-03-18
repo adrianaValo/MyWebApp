b6114d3e953bbb5ecbf72539aa58adb0f958b87f
adrianaValo <adriana.olteanu14@gmail.com>
2024-03-18 18:00:37 +0200

    My JS Codes

---
+++ REACT/yt-clone/yt-clone/src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "c28e881f5fmsh07da099f930997ep1941e3jsn5fc09739323a",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
